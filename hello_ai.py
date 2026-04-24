88d9faf0654969d18c99c28e1c81294d430463de
Miguel <sangmiguel31@gamil.com>
2026-04-24 14:13:58 +0300

    first commit

---
+++ hello_ai.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+load_dotenv()
+client = OpenAI(base_url="https://openrouter.ai/api/v1",api_key=os.getenv("OPENROUTER_API_KEY"))
+question = input("Ask me anything: ")
+response = client.chat.completions.create(model="mistralai/mistral-small-3.1-24b-instruct",messages=[{"role": "system", "content": "You are a helpful codig assistantfor beginner developers.Keep answers simple, friendly and easy to understand."},{"role":"user","content": question}])
+print(response.choices[0].message.content)
